f19fdc73aea15f5f9f9581957917f6602643165b
ktalmage <ktalmage85@gmail.com>
2020-11-01 16:37:41 -0500

    basic navigation set up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-
+import Navigation from './components/Navigation'
 import './App.css';
 
+ 
 function App() {
+
   return (
+    
     <div className="App">
+      <Navigation/>
       <h1>Hi my name is Kyle Talmage- Welcome to my portfolio website.</h1> 
       <p>I was previously a tax accountant working in the hedge fund world but realized<p/>
         <p>I had a passion for technology. After attending the Flatiron School I have been able to</p> 
@@ -11,7 +15,9 @@ function App() {
         to accounting and finance-based problems.</p> 
         <h2>Here are some of my projects: </h2>
       </div>
+
   );
+  
 }
 
 export default App;
---
+++ src/components/Navigation.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import { NavLink } from 'react-router-dom'
+
+
+class Navigation extends Component {
+    render() {
+        <NavLink/>
+        return (
+            <div>
+                  you are cool              
+                
+            </div>
+        )
+    }
+}
+
+export default Navigation
