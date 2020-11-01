53eeba9bd74a850ce543a068071ffdbe047415db
ktalmage <ktalmage85@gmail.com>
2020-11-01 13:43:41 -0500

    basic description added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1>Hi my name is Kyle Talmage- Welcome to my portfolio website.</h1> 
+      <p>I was previously a tax accountant working in the hedge fund world but realized<p/>
+        <p>I had a passion for technology. After attending the Flatiron School I have been able to</p> 
+          bring things full circle. Now I want to focus on applying technology 
+        to accounting and finance-based problems.</p> 
+        <h2>Here are some of my projects: </h2>
+      </div>
   );
 }
 
