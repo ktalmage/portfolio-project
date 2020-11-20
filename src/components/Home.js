63b08e5cd5eac7ae8dc9cd9603200c6feef45f05
ktalmage <ktalmage85@gmail.com>
2020-11-20 12:40:42 -0500

    added click button feature for practice

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,12 +3,14 @@ import { Button } from 'react-bootstrap'
 import { useState } from 'react'
 const Home = () => {
     const [count,setCount] = useState(0)
+    
     return (
         <div>
-            <h1>Click if you are a fan of this site: {count} </h1>
+            <h1>Click if you are a fan of this site: {count}  </h1>
             <Button onClick={() => setCount(count + 1)}>
             Click here!
         </Button>
+            
             <h1>Hi my name is Kyle Talmage- Welcome to my portfolio website.</h1> 
             <p>I was previously a tax accountant working in the hedge fund world but realized<p/>
             <p>I had a passion for technology. After attending the Flatiron School I have been able to</p> 
